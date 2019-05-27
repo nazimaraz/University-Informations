5c4b17705b37c6a7770bf6c7a1e3e48a5c546330
Nax <nzmcnarz@gmail.com>
2019-05-27 18:56:37 +0300

    Adedd comment lines and formatted some columns of university

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -2,6 +2,7 @@ class UniversitiesController < ApplicationController
 
     # GET /universities
     def index
+        # get universities with only id and name columns
         @universities = University.select("id", "name")
         render json: @universities
     end
@@ -9,24 +10,19 @@ class UniversitiesController < ApplicationController
     # GET /universities/:id
     def show
         begin
+            # get university except created_at and updated_at columns, also include students
             @university = University.find(params[:id]).as_json(
                 except: [:created_at, :updated_at],
                 include: {students: {only: [:id, :name, :started_at]}}
             )
+            # format founded_at and started_at of students columns
+            @university["founded_at"] = @university["founded_at"].strftime("%F")
+            @university["students"].each do |student|
+                student["started_at"] = student["started_at"].strftime("%F")
+            end
             render json: @university
         rescue
             render json: {"status": "error", "message": params[:id] + " numaralı üniversite kaydı bulunamadı"}
         end
       end
-
-    # POST /universities
-    def create
-        @university = University.new(university_params)
-        render json: @university, status: :created
-    end
-
-    private
-        def university_params
-            params.require(:university).permit(:api_id, :name, :city, :web_page, :type, :founded_at)
-        end
 end
